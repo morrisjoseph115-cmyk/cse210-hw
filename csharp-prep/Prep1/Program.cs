ecb90bb9c9f0e606586054febff1b91c32c4d165
Joseph <morrisjoseph115@gmail.com>
2026-09-21 09:13:50 -0600

    tweaked the code to make the user inputs cleaner and appear better put together.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+        Console.WriteLine("please enter your first name. :");
+        firstName = Console.ReadLine();
+        Console.ReadLine();
+        Console.Write("please enter your last name. :");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"{firstName} {lastName}");
     }
 }
