e3bb0ecc5041b0c3fba7d56fccd0616dcbef9400
Joseph <morrisjoseph115@gmail.com>
2026-09-22 17:48:15 -0600

    Going to attempt first run to see if this thing has a stroke with the crude logic.

--- csharp-prep/Prep2/C#prep2.cs
+++ csharp-prep/Prep2/C#prep2.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade % you have? ");
+        string useresponse = Console.ReadLine();
+        int gradepercent = int.Parse(useresponse);
+        string lettergrade = "";
+
+        if (gradepercent >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (gradepercent >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (gradepercent >= 70)
+        {
+            lettergrade = "C";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+        Console.WriteLine($"cool, your grade is a{lettergrade}");
     }
 }
