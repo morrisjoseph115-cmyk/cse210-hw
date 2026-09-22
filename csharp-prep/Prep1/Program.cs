74e0fcee7ec6fc7337424d904d09f82f4ec5f5e6
Joseph <morrisjoseph115@gmail.com>
2026-09-22 16:54:37 -0600

    I tweaked the name convention and dialog to match the 007 bond reference. Cheesey I suppose.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,12 +6,12 @@ class Program
     {
         string firstName;
         string lastName;
-        Console.WriteLine("please enter your first name. :");
+        Console.WriteLine("What is your first name?. :");
         firstName = Console.ReadLine();
         Console.ReadLine();
-        Console.Write("please enter your last name. :");
+        Console.Write("What is your last name? :");
         lastName = Console.ReadLine();
 
-        Console.WriteLine($"The full username is :{lastName}_{firstName}");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
